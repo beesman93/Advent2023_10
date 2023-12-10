8a6f4a5384bd847c57bf87b8b6bc0c235ee36d26
beesman93 <chvojtomas@gmail.com>
2023-12-10 14:05:45 +0100

    measure avg duration

--- Program.cs
+++ Program.cs
@@ -1,154 +1,167 @@
 ﻿using System.Diagnostics;
 
 Stopwatch sw = Stopwatch.StartNew();
-
-List<string> lines = new();
-using (StreamReader reader = new(args[0]))
-{
-    while (!reader.EndOfStream)
-    {
-        lines.Add(reader.ReadLine());
-    }
-}
-long readInputTime = sw.ElapsedMilliseconds;
-Console.WriteLine($"readInput in {readInputTime} ms");
-
-List<List<char>> map = new();
-List<List<bool>> boundry = new();
-
-int xOrigin=0, yOrigin=0;
-
-for(int i=0;i<lines.Count;i++)
-{
-    map.Add(new());
-    boundry.Add(new());
-    for(int j=0;j<lines.Count;j++)
-    {
-        map.Last().Add(lines[i][j]);
-        boundry.Last().Add(false);
-        if (map[i][j] == 'S')
-        {
-            xOrigin = i;
-            yOrigin = j;
-        }
-    }
-}
-
-boundry[xOrigin][yOrigin] = true;
-coord prev = new(xOrigin, yOrigin);
-
-bool canGoUp, canGoDown, canGoLeft, canGoRight;
-canGoUp=canGoDown=canGoLeft=canGoRight=false;
-
-if (xOrigin - 1 > 0 &&(
-    map[xOrigin - 1][yOrigin]=='|' ||
-    map[xOrigin - 1][yOrigin] == 'F' ||
-    map[xOrigin - 1][yOrigin] == '7')
-    ) canGoUp=true;
-if (xOrigin + 1 < map.Count && (
-    map[xOrigin + 1][yOrigin] == '|' ||
-    map[xOrigin + 1][yOrigin] == 'L' ||
-    map[xOrigin + 1][yOrigin] == 'J')
-    ) canGoDown = true;
-if (yOrigin - 1 > 0 && (
-    map[xOrigin][yOrigin - 1] == '-' ||
-    map[xOrigin][yOrigin - 1] == 'F' ||
-    map[xOrigin][yOrigin - 1] == 'L')
-    ) canGoLeft = true;
-if (yOrigin + 1 < map[0].Count && (
-    map[xOrigin][yOrigin + 1] == '-' ||
-    map[xOrigin][yOrigin + 1] == '7' ||
-    map[xOrigin][yOrigin + 1] == 'J')
-    ) canGoRight = true;
-
-if (canGoUp && canGoDown) map[xOrigin][yOrigin] = '|';
-if (canGoUp && canGoLeft) map[xOrigin][yOrigin] = 'J';
-if (canGoUp && canGoRight) map[xOrigin][yOrigin] = 'L';
-if (canGoDown && canGoLeft) map[xOrigin][yOrigin] = '7';
-if (canGoDown && canGoRight) map[xOrigin][yOrigin] = 'F';
-if (canGoLeft && canGoRight) map[xOrigin][yOrigin] = '-';
-
-coord firstPath = new(xOrigin, yOrigin);
-if (canGoUp)
-    firstPath.x--;
-else if (canGoDown)
-    firstPath.x++;
-else if (canGoRight)
-    firstPath.y++;
-else
-    throw new Exception("Start should be able to go to 2 points.");
-
+long total = 0;
 
 int pathLen = 0;
-while (firstPath.x!= xOrigin || firstPath.y!= yOrigin)
-{
-    boundry[firstPath.x][firstPath.y] = true;
-
-    char c = map[firstPath.x][firstPath.y];
-    coord newCo = moveNext(c, firstPath, prev, map);
-    prev = firstPath;
-    firstPath = newCo;
-    pathLen++;
-}
-
-Console.WriteLine(Math.Ceiling(pathLen / 2.0));//part 1
-
-for (int i = 0; i < map.Count; i++)
-{
-    for (int j = 0; j < map[i].Count; j++)
-    {
-        if (!boundry[i][j])
-            map[i][j] = '.';
-    }
-}
-
 int points_in = 0;
-Parallel.For (0,map.Count, (i,state)=>
+for (int iters = 0; iters < 1000; iters++)
 {
-    for (int j = 0; j < map[i].Count; j++)
+    
+    sw.Start();
+    pathLen = 0;
+    List<string> lines = new();
+    using (StreamReader reader = new(args[0]))
     {
-        if (!boundry[i][j])
+        while (!reader.EndOfStream)
         {
-            int count_intersects_right = 0;
+            lines.Add(reader.ReadLine());
+        }
+    }
 
-            int jj = j;
-            while (jj < map[i].Count)
+    List<List<char>> map = new();
+    List<List<bool>> boundry = new();
+
+    int xOrigin = 0, yOrigin = 0;
+
+    for (int i = 0; i < lines.Count; i++)
+    {
+        map.Add(new());
+        boundry.Add(new());
+        for (int j = 0; j < lines.Count; j++)
+        {
+            map.Last().Add(lines[i][j]);
+            boundry.Last().Add(false);
+            if (map[i][j] == 'S')
             {
-                if (map[i][jj] == '|')
-                    count_intersects_right++;
-                if (map[i][jj] == 'F' || map[i][jj] == 'L')
-                {
-                    int jjj = jj;
-                    bool isIntersect = false;
-                    while (jjj < map[i].Count)
-                    {
-                        if (map[i][jjj] == '7')
-                        {
-                            isIntersect = map[i][jj] == 'L';
-                            break;
-                        }
-                        if (map[i][jjj] == 'J')
-                        {
-                            isIntersect = map[i][jj] == 'F';
-                            break;
-                        }
-                        jjj++;
-                    }
-                    if(isIntersect)
-                        count_intersects_right++;
-                }
-                jj++;
-            }
-            if (count_intersects_right % 2 == 1)
-            {
-                Interlocked.Increment(ref points_in);
+                xOrigin = i;
+                yOrigin = j;
             }
         }
     }
-});
-Console.WriteLine(points_in); //part 2
 
-Console.WriteLine($"solutions in {sw.ElapsedMilliseconds-readInputTime} ms ({sw.ElapsedMilliseconds} ms total)");
+    boundry[xOrigin][yOrigin] = true;
+    coord prev = new(xOrigin, yOrigin);
+
+    bool canGoUp, canGoDown, canGoLeft, canGoRight;
+    canGoUp = canGoDown = canGoLeft = canGoRight = false;
+
+    if (xOrigin - 1 > 0 && (
+        map[xOrigin - 1][yOrigin] == '|' ||
+        map[xOrigin - 1][yOrigin] == 'F' ||
+        map[xOrigin - 1][yOrigin] == '7')
+        ) canGoUp = true;
+    if (xOrigin + 1 < map.Count && (
+        map[xOrigin + 1][yOrigin] == '|' ||
+        map[xOrigin + 1][yOrigin] == 'L' ||
+        map[xOrigin + 1][yOrigin] == 'J')
+        ) canGoDown = true;
+    if (yOrigin - 1 > 0 && (
+        map[xOrigin][yOrigin - 1] == '-' ||
+        map[xOrigin][yOrigin - 1] == 'F' ||
+        map[xOrigin][yOrigin - 1] == 'L')
+        ) canGoLeft = true;
+    if (yOrigin + 1 < map[0].Count && (
+        map[xOrigin][yOrigin + 1] == '-' ||
+        map[xOrigin][yOrigin + 1] == '7' ||
+        map[xOrigin][yOrigin + 1] == 'J')
+        ) canGoRight = true;
+
+    if (canGoUp && canGoDown) map[xOrigin][yOrigin] = '|';
+    if (canGoUp && canGoLeft) map[xOrigin][yOrigin] = 'J';
+    if (canGoUp && canGoRight) map[xOrigin][yOrigin] = 'L';
+    if (canGoDown && canGoLeft) map[xOrigin][yOrigin] = '7';
+    if (canGoDown && canGoRight) map[xOrigin][yOrigin] = 'F';
+    if (canGoLeft && canGoRight) map[xOrigin][yOrigin] = '-';
+
+    coord firstPath = new(xOrigin, yOrigin);
+    if (canGoUp)
+        firstPath.x--;
+    else if (canGoDown)
+        firstPath.x++;
+    else if (canGoRight)
+        firstPath.y++;
+    else
+        throw new Exception("Start should be able to go to 2 points.");
+
+
+    
+    while (firstPath.x != xOrigin || firstPath.y != yOrigin)
+    {
+        boundry[firstPath.x][firstPath.y] = true;
+
+        char c = map[firstPath.x][firstPath.y];
+        coord newCo = moveNext(c, firstPath, prev, map);
+        prev = firstPath;
+        firstPath = newCo;
+        pathLen++;
+    }
+
+
+
+    for (int i = 0; i < map.Count; i++)
+    {
+        for (int j = 0; j < map[i].Count; j++)
+        {
+            if (!boundry[i][j])
+                map[i][j] = '.';
+        }
+    }
+
+    points_in = 0;
+    Parallel.For(0, map.Count, (i, state) =>
+    {
+        for (int j = 0; j < map[i].Count; j++)
+        {
+            if (!boundry[i][j])
+            {
+                int count_intersects_right = 0;
+
+                int jj = j;
+                while (jj < map[i].Count)
+                {
+                    if (map[i][jj] == '|')
+                        count_intersects_right++;
+                    if (map[i][jj] == 'F' || map[i][jj] == 'L')
+                    {
+                        int jjj = jj;
+                        bool isIntersect = false;
+                        while (jjj < map[i].Count)
+                        {
+                            if (map[i][jjj] == '7')
+                            {
+                                isIntersect = map[i][jj] == 'L';
+                                break;
+                            }
+                            if (map[i][jjj] == 'J')
+                            {
+                                isIntersect = map[i][jj] == 'F';
+                                break;
+                            }
+                            jjj++;
+                        }
+                        if (isIntersect)
+                            count_intersects_right++;
+                    }
+                    jj++;
+                }
+                if (count_intersects_right % 2 == 1)
+                {
+                    Interlocked.Increment(ref points_in);
+                }
+            }
+        }
+    });
+
+    sw.Stop();
+    
+    total += sw.ElapsedMilliseconds;
+    sw.Reset();
+}
+Console.WriteLine(
+    $"part1: {Math.Ceiling(pathLen / 2.0)}\n" +
+    $"part2: {points_in}" +
+    $"\n{total/1000} ms avg");
 
 static coord moveNext(char c, coord curr, coord prev, List<List<char>> map)
 {
